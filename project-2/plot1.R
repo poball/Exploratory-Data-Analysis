f21a4ef8eca57e7ac1e574189027219201aa5309
poball <poball@gmail.com>
2015-08-23 13:25:18 -0400

    Create plot1.R

---
+++ project-2/plot1.R
@@ -0,0 +1,25 @@
+# Exploratory Data Analysis
+#project-2
+
+#setup directory and loading data 
+setwd("./coursera/Exploratory-Data-Analysis/project-2")
+NEI <- readRDS("./summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+# Aggregate
+Emissions <- aggregate(NEI[, 'Emissions'], by=list(NEI$year), FUN=sum)
+Emissions$PM <- round(Emissions[,2]/1000,2)
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
+
+# plot and save in same directory
+png(filename='./plot1.png')
+
+barplot(Emissions$PM, names.arg=Emissions$Group.1, 
+        main=expression('Total Emission of PM'[2.5]),
+        xlab='Year', ylab=expression(paste('PM', ''[2.5], ' in Kilotons')))
+
+dev.off()
